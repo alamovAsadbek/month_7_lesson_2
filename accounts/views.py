69a741fa2cf8060928c3d55d8d3385e9d28f88c4
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-20 13:07:21 +0500

    change views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # O'zingizning home URL'ingizga o'zgartiring
+    return render(request, 'accounts/login.html')
